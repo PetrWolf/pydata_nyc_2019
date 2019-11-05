c44f078894984791d6fdc3601a20aa60febf7bb3
Petr Wolf <1173038+PetrWolf@users.noreply.github.com>
2019-11-04 21:26:22 -0500

    Add exercises for Pylint

---
+++ exercises/bad_code.py
@@ -0,0 +1,15 @@
+# this is a sample of intentionally bad code
+
+import os
+import pandas as pd
+
+def foo(u, v):
+    if u > 0:
+        return u
+    elif v > 0:
+        print(v)
+    else:
+        print(w)
+
+def bar(i, j, k):py
+    return foo(i, j, k) + 1
--- solutions/parsing.py
+++ solutions/parsing.py
@@ -9,10 +9,10 @@ LOCATION_RE = re.compile(r"^([0-9]+(?:\.[0-9]+)?) ([NS]) ([0-9]+(?:\.[0-9]+)?) (
 def parse_location(location: str) -> Tuple[float, float]:
     """Extracts latitude and longitude from a given location string.
 
-    Args: 
-        location: Decimal Degrees (D.D) representation of a geographical location, 
+    Args:
+        location: Decimal Degrees (D.D) representation of a geographical location,
             e.g. "34.56 N 123.45 W"
-    
+
     Returns:
         latitude, longitude
 
@@ -30,7 +30,7 @@ def parse_location(location: str) -> Tuple[float, float]:
     """
     match = LOCATION_RE.match(location)
 
-    if match: 
+    if match:
         latitude_str, north_south, longitude_str, east_west = match.groups()
         latitude, longitude = float(latitude_str), float(longitude_str)
 
@@ -39,5 +39,5 @@ def parse_location(location: str) -> Tuple[float, float]:
                 latitude * (-1 if north_south == "S" else 1),
                 longitude * (-1 if east_west == "W" else 1),
             )
-    
+
     raise ValueError(f'Invalid location "{location}"')
